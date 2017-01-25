512e571f571c43d08f9bee022376f0d2fa459d90
Christian Chung <christian.m.chung@gmail.com>
2017-01-25 14:45:02 -0500

    tests(bootstrap): Fix bootstrap main plugin file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,7 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/adbutler-contributed-creatives.php';
+	require dirname( dirname( __FILE__ ) ) . '/adbutler-user-campaigns.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
